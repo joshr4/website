163d278d0d27d897ae673fa34b8007b9bd5e33d3
Josh Remaley <joshremaley@gmail.com>
2018-08-15 21:08:20 -0500

    added github links

--- client/components/civic/components/portfolio.js
+++ client/components/civic/components/portfolio.js
@@ -11,85 +11,26 @@ export const Portfolio = props => {
 
 	return (
 		<section className="portfolio-section spad pb-0">
-			<div className="container-fluid">
-				<div className="row">
-					<div className="col-xl-4 col-md-8 offset-xl-2 ">
 						<div className="section-title">
 							<h2>Portfolio</h2>
-						</div>
-					</div>
 				</div>
 				<Grid>
 						<Grid.Column mobile={16} tablet={8} computer={5}>
 							<Image src='/img/pack-finder.png' as='a' size='medium' href='https://pack-finder.herokuapp.com/' target='_blank' />
 							<p>Pack-finder</p>
+							<a href="https://github.com/DJ-RM/PackFinder" target="_blank" rel="noopener noreferer"><i className="fa fa-github"> Github Repo</i></a>
 						</Grid.Column >
 						<Grid.Column mobile={16} tablet={8} computer={5}>
 							<Image src='/img/torch-store.png' as='a' size='medium' href='http://torch-store.herokuapp.com/' target='_blank'/>
 							<p>Torch-store</p>
+							<a href="https://github.com/Erikas-firestarters/graceshopper" target="_blank" rel="noopener noreferer"><i className="fa fa-github"> Github Repo</i></a>
 						</Grid.Column>
 						<Grid.Column mobile={16} tablet={8} computer={5}>
 							<Image src='/img/pack-finder.png' as='a' size='medium' href='https://google.com' target='_blank'/>
 							<p>Raspberry-Pi Thermostat</p>
+							<a href="https://github.com/joshr4/stackathon" target="_blank" rel="noopener noreferer"><i className="fa fa-github"> Github Repo</i></a>
 						</Grid.Column>
-					{/* <Grid.Row>
-						<Grid.Column>
-							<p>Pack-finder</p>
-						</Grid.Column>
-						<Grid.Column>
-							<p>torch-store</p>
-						</Grid.Column>
-						<Grid.Column>
-							<p>Pack-finder2</p>
-						</Grid.Column>
-					</Grid.Row> */}
 				</Grid>
-
-				{/* <div className="portfolio-josh">
-					<div className="portfolio-media">
-						<img src="/img/pack-finder.png" className="portfolio-img"/>
-					</div>
-					<div className="portfolio-media">
-						<img src="/img/torch-store.png" className="portfolio-img"/>
-					</div>
-					<div className="portfolio-media">
-						<img src="/img/pack-finder.png" className="portfolio-img"/>
-					</div>
-				</div> */}
-
-				{/* <div className="portfolio-warp">
-				<div className="row">
-					<div className="col-xl-3 col-lg-6 col-md-6">
-						<div className="portfolio-item">
-							<img href="/img/pack-finder.png"/>
-							<h2>Pack-Finder</h2>
-							<p>A for fun social networking app for friendly dog owners.</p>
-						</div>
-					</div>
-					<div className="col-xl-3 col-lg-6 col-md-6">
-						<div className="portfolio-item">
-							<a href="/img/torch-store.png" className="set-bg port-pic" data-setbg="/img/torch-store.png" />
-							<h2>Torch Store</h2>
-							<p>An e-commerce site</p>
-						</div>
-					</div>
-					<div className="col-xl-3 col-lg-6 col-md-6">
-						<div className="portfolio-item">
-							<a href="img/portfolio/3.jpg" className="set-bg port-pic" data-setbg="img/portfolio/3.jpg" />
-							<h2>Raspberry Pi Thermostat</h2>
-							<p>A work in progress thermostat app built with a raspberry pi.</p>
-						</div>
-					</div>
-					<div className="col-xl-3 col-lg-6 col-md-6">
-						<div className="portfolio-item">
-							<a href="img/portfolio/4.jpg" className="set-bg port-pic" data-setbg="img/portfolio/4.jpg" />
-							<h2>Bit Boards</h2>
-							<p>First place hackathon project. Distrbuted Markets Hackathon - Chicago 2018</p>
-						</div>
-					</div>
-				</div>
-			</div> */}
-			</div>
 		</section>
 	)
 }
