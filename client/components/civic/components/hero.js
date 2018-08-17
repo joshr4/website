de6cce808bfe8fff017a32ca3ab39cc04b56b97b
Josh Remaley <joshremaley@gmail.com>
2018-08-16 19:29:55 -0500

    top section tweaks

--- client/components/civic/components/hero.js
+++ client/components/civic/components/hero.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Button } from 'semantic-ui-react'
+import { Button, Image } from 'semantic-ui-react'
 
 /**
  * COMPONENT
@@ -10,14 +10,21 @@ export const Header = props => {
   const { email } = props
 
   return (
-    <section className="container-fluid mobilenopadding hero-padding">
-    <p className="title-josh">Hi, I'm Josh</p>
-      <div>
-        <h4>I’m a full stack developer who has a passion for new technologies. I'm most comfortable writing javascript on the front and back end, but am always open to learning new languages!</h4>
-        <h4>In my past career I was a mechanical engineering consultant. I recently completed an immersive software engineering program at Fullstack Academy. I'm a big proponent of distributed ledger blockchain technologies and will strive to become an expert in the area.</h4>
-        <h4>Technologies: JavaScript(ES6), React, Redux, Node, Express, PostGreSQL, Sequelize, HTML/CSS, Docker, Portainer, Git, FreeNAS, Bitcoin</h4>
+    <div className="master-hero">
+      <div className="headshot-flex">
+        <p className="title-josh">Hi, I'm Josh</p>
+        <div className="headshot-image">
+          <img src="/img/headshot.jpg" />
+        </div>
       </div>
-    </section>
+      <section className="container-fluid mobilenopadding hero-padding">
+        <div>
+          <h4>I’m a full stack developer who has a passion for new technologies. I'm most comfortable with javascript, but always open to learning new languages.</h4>
+          <h4>In my past career I was a mechanical engineering consultant. I recently completed an immersive software engineering program at Fullstack Academy. I'm a big proponent of distributed ledger blockchain technologies and will strive to expand my skills in this area.</h4>
+          <h4>Technologies: JavaScript(ES6), React, Redux, Node, Express, PostGreSQL, Sequelize, HTML/CSS, Docker, Portainer, Git, FreeNAS, Bitcoin</h4>
+        </div>
+      </section>
+    </div>
   )
 }
 
